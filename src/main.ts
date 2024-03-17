ddb8b39fc2d4e9b8ba3f20d74878bb7205f4eb4b
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2024-03-17 22:56:38 +0800

    Feature: registered user arco-design

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import ArcoVue from '@arco-design/web-vue';
+import ArcoVueIcon from '@arco-design/web-vue/es/icon';
+import '@arco-design/web-vue/dist/arco.css';
+import './assets/main.css'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +11,8 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(ArcoVue);
+app.use(ArcoVueIcon);
 app.use(router)
 
 app.mount('#app')
