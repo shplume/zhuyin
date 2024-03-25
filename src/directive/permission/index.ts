85ab4fc039a82b6c3446e37b7f8d068853701212
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2024-03-25 17:02:35 +0800

    Perf: custom directives type

--- src/directive/permission/index.ts
+++ src/directive/permission/index.ts
@@ -1,4 +1,4 @@
-import { DirectiveBinding } from 'vue';
+import type { DirectiveBinding } from 'vue';
 import { useUserStore } from '@/store';
 
 function checkPermission(el: HTMLElement, binding: DirectiveBinding) {
@@ -26,5 +26,5 @@ export default {
   },
   updated(el: HTMLElement, binding: DirectiveBinding) {
     checkPermission(el, binding);
-  },
+  }
 };
