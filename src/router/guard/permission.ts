fe76d6e36bbaeb546f54d7c63b6660b3e90feba4
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2024-03-25 15:36:22 +0800

    Perf: permission information control

--- src/router/guard/permission.ts
+++ src/router/guard/permission.ts
@@ -1,7 +1,21 @@
 import type { Router } from 'vue-router';
 
+import usePermission from '@/hooks/permission';
+import { useUserStore } from '@/store';
+import { appRoutes } from '../routers';
+import { NOT_FOUND } from '../constants';
+
 export default function setupPermissionGuard(router: Router) {
   router.beforeEach(async (to, from, next) => {
-    // ...权限控制
+    const userStore = useUserStore();
+    const Permission = usePermission();
+    const permissionsAllow = Permission.accessRouter(to);
+    if (permissionsAllow) next();
+    else {
+      const destination =
+        Permission.findFirstPermissionRoute(appRoutes, userStore.role) ||
+        NOT_FOUND;
+      next(destination);
+    }
   });
 }
