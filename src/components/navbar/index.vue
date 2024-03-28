9ffcc364a2a34d43b44bc70f7693c15844ba4a2b
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2024-03-28 19:07:53 +0800

    Style: components navbar

--- src/components/navbar/index.vue
+++ src/components/navbar/index.vue
@@ -137,6 +137,8 @@
     display: flex;
     justify-content: space-between;
     height: 100%;
+    background-color: var(--color-bg-2);
+    border-bottom: 1px solid var(--color-border);
   }
 
   .left-side {
