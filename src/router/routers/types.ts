dc3acab2740b77515f9d30a3c45bc53db3927183
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2024-03-28 18:32:39 +0800

    Perf: routers type

--- src/router/routers/types.ts
+++ src/router/routers/types.ts
@@ -1,4 +1,5 @@
 import { defineComponent } from 'vue';
+import type { RouteMeta, NavigationGuard } from 'vue-router';
 
 export type Component<T = any> =
   | ReturnType<typeof defineComponent>
@@ -8,5 +9,12 @@ export type Component<T = any> =
 export interface AppRouteRecordRaw {
   path: string;
   name?: string | symbol;
+  meta?: RouteMeta;
+  redirect?: string;
   component: Component | string;
+  children?: AppRouteRecordRaw[];
+  alias?: string | string[];
+  props?: Record<string, any>;
+  beforeEnter?: NavigationGuard | NavigationGuard[];
+  fullPath?: string;
 }
