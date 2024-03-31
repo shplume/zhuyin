8bf2c15404adf00aa0c97972846e0966e024f32f
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2024-03-31 16:18:44 +0800

    Fix: page my thesis

---
+++ src/views/my-thesis/thesis-state/components/operation-log.vue
@@ -0,0 +1,62 @@
+<template>
+  <a-card class="general-card" :title="'论文评阅记录'">
+    <a-spin :loading="loading" style="width: 100%">
+      <a-table :data="renderData">
+        <template #columns>
+          <a-table-column :title="'操作人'" data-index="contentNumber" />
+          <a-table-column :title="'操作内容'" data-index="updateContent" />
+          <a-table-column :title="'当前状态'" data-index="status">
+            <template #cell="{ record }">
+              <p v-if="record.status === 0">
+                <span class="circle"></span>
+                <span>{{ $t('basicProfile.cell.auditing') }}</span>
+              </p>
+              <p v-if="record.status === 1">
+                <span class="circle pass"></span>
+                <span>{{ $t('basicProfile.cell.pass') }}</span>
+              </p>
+            </template>
+          </a-table-column>
+          <a-table-column :title="'操作时间'" data-index="updateTime" />
+          <a-table-column :title="'操作'">
+            <template #cell>
+              <a-button type="text">查看</a-button>
+            </template>
+          </a-table-column>
+        </template>
+      </a-table>
+    </a-spin>
+  </a-card>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import { queryOperationLog, operationLogRes } from '@/api/profile';
+  import useLoading from '@/hooks/loading';
+
+  const { loading, setLoading } = useLoading(true);
+  const renderData = ref<operationLogRes>([]);
+  const fetchData = async () => {
+    try {
+      const { data } = await queryOperationLog();
+      renderData.value = data;
+
+      window.console.log(renderData.value);
+    } catch (err) {
+      // you can report use errorHandler or other
+    } finally {
+      setLoading(false);
+    }
+  };
+  fetchData();
+</script>
+
+<style scoped lang="less">
+  :deep(.arco-table-th) {
+    &:last-child {
+      .arco-table-th-item-title {
+        margin-left: 16px;
+      }
+    }
+  }
+</style>
--- src/views/my-thesis/thesis-state/index.vue
+++ src/views/my-thesis/thesis-state/index.vue
@@ -1,25 +1,80 @@
 <template>
   <div class="container">
     <Breadcrumb :items="['menu.myThesis', 'menu.myThesis.state']" />
+    <a-space direction="vertical" :size="16" fill>
+      <a-card class="general-card" :title="'论文评审流程'">
+        <template #extra>
+          <a-space>
+            <a-button type="primary">{{ '取消评审流程' }}</a-button>
+          </a-space>
+        </template>
+        <a-steps v-model:current="step" line-less class="steps">
+          <a-step>{{ '论文提交' }}</a-step>
+          <a-step>{{ '评阅中' }}</a-step>
+          <a-step>{{ '评阅完成' }}</a-step>
+        </a-steps>
+      </a-card>
+      <a-card class="general-card">
+        <ProfileItem :loading="loading" :render-data="currentData" />
+      </a-card>
+      <a-card class="general-card">
+        <ProfileItem :loading="preLoading" type="pre" :render-data="preData" />
+      </a-card>
+      <OperationLog />
+    </a-space>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import useLoading from '@/hooks/loading';
+  import { queryProfileBasic, ProfileBasicRes } from '@/api/profile';
+  import ProfileItem from './components/profile-item.vue';
+  import OperationLog from './components/operation-log.vue';
+
+  const { loading, setLoading } = useLoading(true);
+  const { loading: preLoading, setLoading: preSetLoading } = useLoading(true);
+  const currentData = ref<ProfileBasicRes>({} as ProfileBasicRes);
+  const preData = ref<ProfileBasicRes>({} as ProfileBasicRes);
+  const step = ref(1);
+  const fetchCurrentData = async () => {
+    try {
+      const { data } = await queryProfileBasic();
+      currentData.value = data;
+      step.value = 2;
+    } catch (err) {
+      // you can report use errorHandler or other
+    } finally {
+      setLoading(false);
+    }
+  };
+  const fetchPreData = async () => {
+    try {
+      const { data } = await queryProfileBasic();
+      preData.value = data;
+    } catch (err) {
+      // you can report use errorHandler or other
+    } finally {
+      preSetLoading(false);
+    }
+  };
+  fetchCurrentData();
+  fetchPreData();
+</script>
+
+<script lang="ts">
+  export default {
+    name: 'Basic',
+  };
+</script>
 
 <style scoped lang="less">
   .container {
-    padding: 0 20px 40px 20px;
-    overflow: hidden;
+    padding: 0 20px 20px 20px;
   }
 
-  .actions {
-    position: fixed;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    height: 60px;
-    padding: 14px 20px 14px 0;
-    background: var(--color-bg-2);
-    text-align: right;
+  .steps {
+    max-width: 548px;
+    margin: 0 auto 10px;
   }
 </style>
