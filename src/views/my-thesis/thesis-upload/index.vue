045b5089dcb31bf481ff1acdb2feb9a2b0fcdb2e
qhbprogrammer <2938263279@qq.com>
2024-04-28 21:30:51 +0800

    Fix: update thesis upload UI

--- src/views/my-thesis/thesis-upload/index.vue
+++ src/views/my-thesis/thesis-upload/index.vue
@@ -4,17 +4,31 @@
     <a-form ref="formRef" layout="vertical" :model="formData">
       <a-space direction="vertical" :size="16">
         <a-card class="general-card">
-          <template #title> 论文信息 </template>
+          <template #title>论文信息</template>
           <a-row :gutter="80">
             <a-col :span="8">
               <a-form-item
-                :label="'论文标题'"
-                field="title"
-                :rules="[{ required: true, message: '论文标题不能为空' }]"
+                :label="'中文标题'"
+                field="title_chinese"
+                :rules="[{ required: true, message: '中文标题不能为空' }]"
                 :validate-trigger="['change', 'blur']"
               >
                 <a-input
-                  v-model="formData.title"
+                  v-model="formData.title_chinese"
+                  :placeholder="'请在这里输入论文标题'"
+                >
+                </a-input>
+              </a-form-item>
+            </a-col>
+            <a-col :span="8">
+              <a-form-item
+                :label="'英文标题'"
+                field="title_english"
+                :rules="[{ required: true, message: '英文论文标题不能为空' }]"
+                :validate-trigger="['change', 'blur']"
+              >
+                <a-input
+                  v-model="formData.title_english"
                   :placeholder="'请在这里输入论文标题'"
                 >
                 </a-input>
@@ -27,18 +41,32 @@
                 :rules="[{ required: true, message: '论文作者不能为空' }]"
                 :validate-trigger="['change', 'blur']"
               >
-                <a-input
+                <a-input-tag
                   v-model="formData.author"
                   :placeholder="'请在这里输入论文作者'"
                 >
-                </a-input>
+                </a-input-tag>
+              </a-form-item>
+            </a-col>
+            <a-col :span="8">
+              <a-form-item
+                :label="'指导教师'"
+                field="teacher"
+                :rules="[{ required: true, message: '指导教师不能为空' }]"
+                :validate-trigger="['change', 'blur']"
+              >
+                <a-input-tag
+                  v-model="formData.teacher"
+                  :placeholder="'请在这里输入指导教师'"
+                >
+                </a-input-tag>
               </a-form-item>
             </a-col>
           </a-row>
         </a-card>
         <a-card class="general-card">
-          <template #title> 论文上传 </template>
-          <a-form-item :label="'具体说明'" field="file">
+          <template #title>论文上传</template>
+          <a-form-item :label="'请上传规定格式的论文'" field="file">
             <a-upload
               v-model:file-list="formData.files"
               draggable
@@ -47,23 +75,72 @@
           </a-form-item>
         </a-card>
         <a-card class="general-card" :bordered="false">
-          <template #title> 论文简介 </template>
+          <template #title>论文的主要创新点</template>
           <a-form-item
-            :label="'具体说明'"
-            field="synopsis"
-            :rules="[{ required: true, message: '论文简介不能为空' }]"
+            :label="'第一点'"
+            field="advantage"
+            :rules="[{ required: true, message: '论文的第一创新点不能为空' }]"
             :validate-trigger="['change', 'blur']"
           >
             <a-textarea
-              v-model="formData.synopsis"
+              v-model="formData.advantage_first"
+              :max-length="200"
+              show-word-limit
               :placeholder="'请简单说明，最多不要超多200字。'"
+            />
+          </a-form-item>
+          <a-form-item
+            :label="'第二点'"
+            field="advantage"
+            :rules="[{ required: true, message: '论文的第二创新点不能为空' }]"
+            :validate-trigger="['change', 'blur']"
+          >
+            <a-textarea
+              v-model="formData.advantage_second"
+              :max-length="200"
+              show-word-limit
+              :placeholder="'请简单说明，最多不要超多200字。'"
+            />
+          </a-form-item>
+          <a-form-item
+            :label="'第三点'"
+            field="advantage"
+            :rules="[{ required: true, message: '论文的第三创新点不能为空' }]"
+            :validate-trigger="['change', 'blur']"
+          >
+            <a-textarea
+              v-model="formData.advantage_third"
+              :max-length="200"
+              show-word-limit
+              :placeholder="'请简单说明，最多不要超多200字。'"
+            />
+          </a-form-item>
+        </a-card>
+        <a-card class="general-card" :bordered="false">
+          <template #title>论文存在的不足之处及改进设想</template>
+          <a-form-item
+            :label="'仅罗列要点'"
+            field="drawback"
+            :rules="[
+              {
+                required: true,
+                message: '论文存在的不足之处及改进设想不能为空',
+              },
+            ]"
+            :validate-trigger="['change', 'blur']"
+          >
+            <a-textarea
+              v-model="formData.drawback"
+              :max-length="400"
+              show-word-limit
+              :placeholder="'请简单说明，最多不要超多400字。'"
             />
           </a-form-item>
         </a-card>
       </a-space>
       <div class="actions">
         <a-space>
-          <a-button> 重置 </a-button>
+          <a-button>重置</a-button>
           <a-button type="primary" :loading="loading" @click="onSubmitClick">
             提交
           </a-button>
@@ -78,10 +155,15 @@
   import useLoading from '@/hooks/loading';
 
   const formData = ref({
-    title: '',
-    author: '',
+    title_chinese: '',
+    title_english: '',
+    author: [],
+    teacher: [],
     files: [],
-    synopsis: '',
+    advantage_first: '',
+    advantage_second: '',
+    advantage_third: '',
+    drawback: '',
   });
 
   const formRef = ref();
