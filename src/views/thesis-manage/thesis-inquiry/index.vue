92ed4dc6797b7e9caae510fc1906d5493e1dd953
ZEQUANR <64631081+ZEQUANR@users.noreply.github.com>
2024-05-24 16:22:43 +0800

    Fix: page thesis list modal

--- src/views/thesis-manage/thesis-inquiry/index.vue
+++ src/views/thesis-manage/thesis-inquiry/index.vue
@@ -92,7 +92,7 @@
           :span="12"
           style="display: flex; align-items: center; justify-content: end"
         >
-          <a-dropdown-button type="primary" @click="() => {}">
+          <a-dropdown-button type="primary" @click="submitConfirm">
             <icon-send style="margin-right: 8px" />
             送审
             <template #content>
@@ -180,6 +180,11 @@
             record.select = !record.select;
           }
         "
+        @selection-change="
+          (rowKeys) => {
+            selectIds = rowKeys;
+          }
+        "
       >
         <template #columns>
           <a-table-column :title="'学号'" data-index="number">
@@ -217,32 +222,51 @@
               {{ record.fileState }}
             </template>
           </a-table-column>
-          <a-table-column :title="'操作'" data-index="status">
+          <a-table-column :title="'操作'" data-index="operate">
             <template #cell="{ record }">
-              {{ record.status }}
+              {{ record.id }}
             </template>
           </a-table-column>
         </template>
       </a-table>
     </a-card>
 
-    <!-- <a-modal v-model:visible="visible" @ok="handleOk" @cancel="handleCancel">
+    <a-modal
+      v-model:visible="isVisible"
+      :width="814"
+      @ok="() => {}"
+      @cancel="() => {}"
+    >
       <template #title> 请选择当前需要评阅的老师 </template>
-      <a-space size="large">
-        <a-avatar>A</a-avatar>
-        <a-avatar :style="{ backgroundColor: '#3370ff' }">
-          <IconUser />
-        </a-avatar>
-        <a-avatar :style="{ backgroundColor: '#14a9f8' }">Arco</a-avatar>
-        <a-avatar :style="{ backgroundColor: '#00d0b6' }">Design</a-avatar>
-        <a-avatar>
-          <img
-            alt="avatar"
-            src="https://p1-arco.byteimg.com/tos-cn-i-uwbnlip3yd/3ee5f13fb09879ecb5185e440cef6eb9.png~tplv-uwbnlip3yd-webp.webp"
-          />
-        </a-avatar>
-      </a-space>
-    </a-modal> -->
+      <a-radio-group>
+        <a-space wrap>
+          <template v-for="item in 4" :key="item">
+            <a-radio :value="item">
+              <template #radio="{ checked }">
+                <a-space
+                  :align="'start'"
+                  class="custom-radio-card"
+                  :class="{ 'custom-radio-card-checked': checked }"
+                  wrap
+                >
+                  <div className="custom-radio-card-mask">
+                    <div className="custom-radio-card-mask-dot" />
+                  </div>
+                  <div>
+                    <div className="custom-radio-card-title">
+                      radio Card {{ item }}
+                    </div>
+                    <a-typography-text type="secondary">
+                      this is a text
+                    </a-typography-text>
+                  </div>
+                </a-space>
+              </template>
+            </a-radio>
+          </template>
+        </a-space>
+      </a-radio-group>
+    </a-modal>
   </div>
 </template>
 
@@ -452,6 +476,13 @@
   };
   const reset = () => {
     formModel.value = generateFormModel();
+    renderData.value = allotListData.value;
+  };
+
+  const isVisible = ref(false);
+  const submitConfirm = () => {
+    isVisible.value = true;
+    window.console.log(selectIds.value);
   };
 
   watch(
@@ -494,5 +525,67 @@
       margin-left: 12px;
       cursor: pointer;
     }
+  }
+
+  .custom-radio-card {
+    padding: 10px 16px;
+    border: 1px solid var(--color-border-2);
+    border-radius: 4px;
+    width: 250px;
+    box-sizing: border-box;
+  }
+
+  .custom-radio-card-mask {
+    height: 14px;
+    width: 14px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 100%;
+    border: 1px solid var(--color-border-2);
+    box-sizing: border-box;
+  }
+
+  .custom-radio-card-mask-dot {
+    width: 8px;
+    height: 8px;
+    border-radius: 100%;
+  }
+
+  .custom-radio-card-title {
+    color: var(--color-text-1);
+    font-size: 14px;
+    font-weight: bold;
+    margin-bottom: 8px;
+  }
+
+  .custom-radio-card:hover,
+  .custom-radio-card-checked,
+  .custom-radio-card:hover .custom-radio-card-mask,
+  .custom-radio-card-checked .custom-radio-card-mask {
+    border-color: rgb(var(--primary-6));
+  }
+
+  .custom-radio-card-checked {
+    background-color: var(--color-primary-light-1);
+  }
+
+  .custom-radio-card:hover .custom-radio-card-title,
+  .custom-radio-card-checked .custom-radio-card-title {
+    color: rgb(var(--primary-6));
+  }
+
+  .custom-radio-card-checked .custom-radio-card-mask-dot {
+    background-color: rgb(var(--primary-6));
+  }
+</style>
+
+<style>
+  .arco-radio-group .arco-radio {
+    margin-right: 0px;
+  }
+
+  .arco-radio {
+    padding-left: 0px;
   }
 </style>
